a5a4d1a0c8b111cc91e9b57a4c49b44a895f8204
yuebuqun <yuebuqun.com>
2024-04-23 17:16:27 +0800

    create jianfa

---
+++ src/main/java/Dugujiujian.java
@@ -0,0 +1,8 @@
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("lalalal11111");
+        System.out.println("lalalal22222");
+        System.out.println("lalalal3333");
+        System.out.println("lalalal44444");
+    }
+}
